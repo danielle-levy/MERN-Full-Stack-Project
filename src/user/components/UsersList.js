fb4ceb03e4fae67ad3c8d2bddf3d8e9bba23ecd1
Danielle Levy <danielle.levy98@gmail.com>
2023-02-02 16:04:39 +0200

    adding card to no users state

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import './UsersList.css'
+import Card from '../../shared/components/UIElements/Card';
 import UserItem from './UserItem'
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return <div className="center">
-            <h2>No Users Found.</h2>
+            <Card>
+                <h2>No Users Found.</h2>
+            </Card>
         </div>;
     }
 
